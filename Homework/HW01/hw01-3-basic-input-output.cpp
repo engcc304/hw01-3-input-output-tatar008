b4591631f5ddb2bc6e398628988b2288975255b6
tatar008 <jirapar_ja66@live.rmutl.ac.th>
2023-07-28 22:57:42 +0700

    hw01-3

--- Homework/HW01/hw01-3-basic-input-output.cpp
+++ Homework/HW01/hw01-3-basic-input-output.cpp
@@ -15,13 +15,13 @@
 */
 #include <stdio.h>
 int main() {
-    char *first ;
-    char *last ;
+    char first[55] ;
+    char last[55] ;
     printf("First Name: ") ;
-    scanf("%c",&first) ;
+    scanf("%s",first) ;
     printf("Last Name: ") ;
-    scanf("%c",&last) ;
-    printf("%c %c ",first,last) ;
+    scanf("%s",last) ;
+    printf("%s %s ",first,last) ;
     printf("TC, RMUTL, Chiang Mai, Thailand" ) ;
     return 0;
 }
